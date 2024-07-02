113f609de20df6c600c335206311a23438f6d6f8
Manohisoa <manohisoarajaonarivony@gmail.com>
2024-07-02 14:47:06 +0300

    added main elements and styling to home page

--- src/components/sections/HomeSection.tsx
+++ src/components/sections/HomeSection.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import transparent from "../../images/transparent.png"
 
 function HomeSection() {
   return (
-    <div className='section' id='home'>HomeSection</div>
+    <div className='section' id='home'>
+      <div className="home-content">
+        <h1>
+        Votre Partenaire de Palettes à Madagascar
+        </h1>
+        <p>
+          Faites confiance à Palette Mada pour des palettes durables et diversifiées, parfaitement adaptées à vos exigences.
+        </p>
+      </div>
+      <div className="home-img">
+        <img src={transparent} alt="Palette Euro" />
+      </div>
+    </div>
   )
 }
 

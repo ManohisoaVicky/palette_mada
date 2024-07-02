3bb844d5eaf6f26cdf42202b829c955da8c847f2
Manohisoa <manohisoarajaonarivony@gmail.com>
2024-07-02 14:57:42 +0300

    added images for about section

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function AboutSection() {
   return (
-    <div className='section' id='about'>AboutSection</div>
+    <div className='section' id='about'>
+      <div className="intro">
+        <h2>Qui Sommes-Nous ?</h2>
+      </div>
+      <div className="history"></div>
+      <div className="values"></div>
+    </div>
   )
 }
 

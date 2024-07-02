9c1dec0b5c42018719ab5783c0d89ea69678239e
Manohisoa <manohisoarajaonarivony@gmail.com>
2024-07-02 10:21:34 +0300

    styled navbar layout

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
   return (
     <div className="navbar">
-      <div className='title-container'>
+      <div className="navbar-container">
+      <div className="title-container">
         <p>Palette Mada</p>
       </div>
-      <div className='navbar-menu'>
-        
+      <div className="navbar-menu">
+        <ul className="navbar-menu-list">
+          <li>Home</li>
+          <li>About</li>
+          <li>Products</li>
+          <li>Contact</li>
+        </ul>
+      </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

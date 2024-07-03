d233d854c9aa2acfd95ae6007c8d9fac09c9ff39
Manohisoa <manohisoarajaonarivony@gmail.com>
2024-07-03 10:56:30 +0300

    added the main style layour for product section

--- src/components/sections/ProductSection.tsx
+++ src/components/sections/ProductSection.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import bois from "../../images/pallete_bois.jpg"
+import plastic from "../../images/pallete_plastic.jpg"
 
 function ProductSection() {
   return (
-    <div className='section' id='product'>ProductSection</div>
+    <div className='section' id='product'>
+      <h2>Nos Produits</h2>
+
+      <div className="product-container">
+
+        <div className="wood-product">
+          <h3>Palette en Bois</h3>
+          <img src={bois} alt="Palette" />
+        </div>
+
+        <div className="plastic-product">
+          <h3>Palette en Plastique</h3>
+          <img src={plastic} alt="Palette" />
+        </div>
+
+      </div>
+      
+    </div>
   )
 }
 
